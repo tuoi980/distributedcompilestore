6e2e618fbc187484c9a9f67954930bff3d5d273d
tuoi980 <fer14pradopun0suy@hotmail.com>
2024-05-08 07:13:50 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-function maximalSquare(matrix) {
-  if (matrix.length === 0 || matrix[0].length === 0) return 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  const dp = Array.from({ length: rows + 1 }, () =>
-    Array.from({ length: cols + 1 }, () => 0),
-  );
-  let maxSquare = 0;
-  for (let i = 1; i <= rows; i++) {
-    for (let j = 1; j <= cols; j++) {
-      if (matrix[i - 1][j - 1] === "1") {
-        dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
-        maxSquare = Math.max(maxSquare, dp[i][j]);
-      }
-    }
+function mergeKLists(lists) {
+  if (lists.length === 0) return null;
+  while (lists.length > 1) {
+    const first = lists.shift();
+    const second = lists.shift();
+    const merged = mergeTwoLists(first, second);
+    lists.push(merged);
   }
-  return maxSquare * maxSquare;
+  return lists[0];
 }
