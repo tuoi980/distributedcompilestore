05c9112a96ee73ffc46f8beb3b16b3dfcf3ce2bf
tuoi980 <fer14pradopun0suy@hotmail.com>
2024-05-12 08:42:07 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function isSubsequence(s, t) {
-  let i = 0;
-  let j = 0;
-  while (i < s.length && j < t.length) {
-    if (s[i] === t[j]) i++;
-    j++;
+function generate(numRows) {
+  const triangle = [];
+  for (let i = 0; i < numRows; i++) {
+    const row = new Array(i + 1).fill(1);
+    for (let j = 1; j < row.length - 1; j++) {
+      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+    }
+    triangle.push(row);
   }
-  return i === s.length;
+  return triangle;
 }
