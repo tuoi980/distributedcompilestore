7840256c7dfecf19cf476b52fff54e20f15d91a4
tuoi980 <fer14pradopun0suy@hotmail.com>
2024-05-15 07:34:25 +0700

    test

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-const uniqueValues = (arr) => [...new Set(arr)];
+const originalObject = { name: "Alice", age: 30 };
+const clonedObject = JSON.parse(JSON.stringify(originalObject));
+console.log(clonedObject); // { name: "Alice", age: 30 }
